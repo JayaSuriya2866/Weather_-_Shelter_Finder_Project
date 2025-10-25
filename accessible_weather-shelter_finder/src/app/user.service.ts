86a1f9b2425da8ec3af0dac0272b725b30056f31
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-10-25 15:04:05 -0500

    User and the weather service is created with respective api's

--- accessible_weather-shelter_finder/src/app/user.service.ts
+++ accessible_weather-shelter_finder/src/app/user.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  // private api_key: string = "";
+
+  private base_url: string = ""
+
+
+  //Save User
+  saveusers(data: any): Observable<any> {
+    console.log(data)
+    return this.http.post(this.base_url + '/users', data);
+  }
+
+  isLoggedIn: boolean = false;
+  //User Login
+  loginUser(logData: any): Observable<any> {
+    console.log(logData)
+    this.isLoggedIn = true;
+    return this.http.post<any>(this.base_url + '/login', logData);
+  }
+
+  //UserLogout
+  logout() {
+    this.isLoggedIn = false;
+  }
+
+  //Update User 
+  updateUser(data: any): Observable<any> {
+    return this.http.put(this.base_url + '/users', data)
+  }
+  
+  isAuthenticated: boolean = false;
+
+  res: string | undefined
+
+  checkAuthenticate() {
+    this.res = 'Activate'
+    console.log(localStorage.getItem('status'))
+    if (localStorage.getItem('status') == this.res) {
+      this.isAuthenticated = true
+    } else {
+      this.isAuthenticated = false
+    }
+  }
 }
--- accessible_weather-shelter_finder/src/app/weather.service.ts
+++ accessible_weather-shelter_finder/src/app/weather.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,31 @@ import { Injectable } from '@angular/core';
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+
+  private api_key: string = "1a4335255c134102883182138251810";
+
+  private base_url: string = "http://api.weatherapi.com/v1/";
+  
+
+  // Get Weather by location (latitude and longitude)
+  getWeatherByLocation(lat: number, lon: number) {
+    return this.httpclient.get(`${this.base_url}current.json?key=${this.api_key}&q=${lat},${lon}`);
+  }
+
+  //Get Current Weather
+  getCurrentWeatherByCity(cityName: string) {
+    return this.httpclient.get(`${this.base_url}current.json?key=${this.api_key}&q=${cityName}`);
+  }
+
+  //Get Weather Forecast
+  getWeatherForecastByCity(cityName: string, days: number) {
+    return this.httpclient.get(`${this.base_url}forecast.json?key=${this.api_key}&q=${cityName}&days=${days}`);
+  }
+
+  //Get Weather Alerts
+  getWeatherAlertsByCity(cityName: string) {
+    return this.httpclient.get(`${this.base_url}alerts.json?key=${this.api_key}&q=${cityName}&alerts=yes`);
+  }
+
 }
